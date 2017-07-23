061a86c75498bdaa6c1331327300faf4e9182f7e
RemyaSelvam <30317816+RemyaSelvam@users.noreply.github.com>
2017-07-23 10:18:33 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+ int n,num,rev=0;
+ printf("Enter any number");
+ scanf("%d",&n);
+ num=n;
+ while(n!=0)
+ {
+  rev=(rev*10)+(n%10);
+  n=n/10;
+ }
+  if(rev==num)
+  {
+   printf("%d is a polindrome",num);
+   }
+   else
+   {
+    printf("%d is not a polindrome",num);
+   }
+    return 0;
+}
