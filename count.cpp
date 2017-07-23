cb4b47c9986710f06a004081258063a1472f32b5
RemyaSelvam <30317816+RemyaSelvam@users.noreply.github.com>
2017-07-23 09:44:07 +0530

    Create count.cpp

---
+++ count.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+ int no,a=0;
+ clrscr();
+ cout<<"Enter any number:";
+ cin>>no;
+ while(no>0)
+ {
+  no=no/10;
+  a++
+  }
+  cout<<"No.of digits in given numer";
+  getch();
+  }
