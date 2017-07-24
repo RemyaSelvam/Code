0e1478e5eea467694acd35971bd3196204ceba1a
RemyaSelvam <30317816+RemyaSelvam@users.noreply.github.com>
2017-07-24 10:10:54 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void main()
+       {
+           int n, s=0, i;
+           cout<<"Enter any number : ";
+           cin>>n;
+           {  
+            for(i=0; i<=n; i++)
+            s+=i;
+           }
+           
+           cout<<"Sum of all numbers up to "<<num<<" is "<<sum << endl;
+        }
